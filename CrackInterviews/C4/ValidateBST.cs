81db9e3d99765187cb912ebfddc07d764347e789
Haoming Yuan <haoming.yuan@cyara.com>
2020-11-10 22:15:58 +1100

    Fixed BST validation

--- CrackInterviews/C4/ValidateBST.cs
+++ CrackInterviews/C4/ValidateBST.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace C4
@@ -9,16 +10,28 @@ namespace C4
         {
             if (root == null) return false;
 
-            return IsBST(root);
+            return IsBST(root, int.MinValue, int.MaxValue);
         }
 
-        internal static bool IsBST(BinaryTreeNode<int> node)
+        /// <summary>
+        /// Check if the current node comply the BST constraints
+        /// </summary>
+        /// <param name="node"></param> Current node
+        /// <param name="min"></param> Data of current node must be greater than min
+        /// <param name="max"></param> Data of current node must be smaller or equal to max
+        /// <returns>True if the current node and its children meet all BST constraints </returns>
+        internal static bool IsBST(BinaryTreeNode<int> node, int min, int max)
         {
             if (node == null) return true;
+            Console.WriteLine($"current data is: {node.Data}; current min: {min}; current max: {max}" );
 
-            return (node.LeftNode == null || node.LeftNode.Data <= node.Data) &&
-                   (node.RightNode == null || node.RightNode.Data > node.Data) && IsBST(node.LeftNode) &&
-                   IsBST(node.RightNode);
+            if (node.Data <= min || node.Data > max)
+            {
+                Console.WriteLine($"data {node.Data} is returning false");
+                return false;
+            }
+
+            return IsBST(node.LeftNode, min, Math.Min(node.Data, max)) && IsBST(node.RightNode, Math.Max(node.Data, min), max);
         }
     }
 
@@ -35,20 +48,30 @@ namespace C4
         {
             yield return new TestCaseData(null, false);
             yield return new TestCaseData(new BinaryTreeNode<int>(1), true);
-
+            
             var node1 = new BinaryTreeNode<int>(1) {LeftNode = new BinaryTreeNode<int>(0)};
             yield return new TestCaseData(node1, true);
-
+            
             var node2 = new BinaryTreeNode<int>(1) {LeftNode = new BinaryTreeNode<int>(2)};
             yield return new TestCaseData(node2, false);
-
+            
             var node3 = new BinaryTreeNode<int>(1)
                 {LeftNode = new BinaryTreeNode<int>(1), RightNode = new BinaryTreeNode<int>(2)};
             yield return new TestCaseData(node3, true);
-
+            
             var node4 = new BinaryTreeNode<int>(1)
                 {LeftNode = new BinaryTreeNode<int>(1), RightNode = new BinaryTreeNode<int>(1)};
             yield return new TestCaseData(node4, false);
+
+            var node5 = new BinaryTreeNode<int>(9)
+            {
+                LeftNode = new BinaryTreeNode<int>(1)
+                {
+                    RightNode = new BinaryTreeNode<int>(20)
+                },
+                RightNode = new BinaryTreeNode<int>(10)
+            };
+            yield return new TestCaseData(node5, false);
         }
     }
 }
