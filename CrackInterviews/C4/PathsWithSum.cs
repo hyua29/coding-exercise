362d682d07247d4394156a6286fad9ccb4639de3
haoming.yuan <haominghyua29@gmail.com>
2020-11-15 12:32:53 +1100

    Added PathsWithSum

---
+++ CrackInterviews/C4/PathsWithSum.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace C4
+{
+    internal class PathsWithSum
+    {
+        internal static int Calculate(BinaryTreeNode<int> root, int sum)
+        {
+            if (root == null) return 0;
+
+
+            return GetPaths(root, sum);
+        }
+
+        private static int GetPaths(BinaryTreeNode<int> node, int sum)
+        {
+            if (node == null) return 0;
+
+            int pathFromCurrent = GetPathsFromNode(node, sum, 0);
+
+            int pathFromLeft = GetPaths(node.LeftNode, sum);
+            int pathFromRight = GetPaths(node.RightNode, sum);
+
+            return pathFromCurrent + pathFromLeft + pathFromRight;
+        }
+
+        private static int GetPathsFromNode(BinaryTreeNode<int> node, int targerSum, int currentSum)
+        {
+            if (node == null) return 0;
+            
+            int newSum = currentSum + node.Data;
+            
+            int pathCount = 0;
+            if (newSum == targerSum) pathCount++;
+
+            pathCount += GetPathsFromNode(node.LeftNode, targerSum, newSum);
+            pathCount += GetPathsFromNode(node.RightNode, targerSum, newSum);
+
+            return pathCount;
+        }
+
+    }
+
+    [TestFixture]
+    public class PathsWithSumTests
+    {
+        [TestCaseSource(nameof(GetTestData))]
+        public void Calculate_Test(BinaryTreeNode<int> root, int sum, int expectedResult)
+        {
+            Assert.That(PathsWithSum.Calculate(root, sum), Is.EqualTo(expectedResult));
+        }
+
+        private static IEnumerable<TestCaseData> GetTestData()
+        {
+            yield return new TestCaseData(null, 8, 0);
+            yield return new TestCaseData(new BinaryTreeNode<int>(10)
+            {
+                LeftNode = new BinaryTreeNode<int>(5)
+                {
+                    LeftNode = new BinaryTreeNode<int>(3)
+                    {
+                        LeftNode = new BinaryTreeNode<int>(3),
+                        RightNode = new BinaryTreeNode<int>(-2)
+                    },
+                    RightNode = new BinaryTreeNode<int>(2)
+                    {
+                        RightNode = new BinaryTreeNode<int>(1)
+                    }
+                }, 
+                RightNode = new BinaryTreeNode<int>(-3) { RightNode = new BinaryTreeNode<int>(11) }
+            }, 8, 3);
+        }
+    }
+}
