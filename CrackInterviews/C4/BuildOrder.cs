1e89b7e376e147471d7d3a98e7d281524293d13d
haoming.yuan <haominghyua29@gmail.com>
2020-11-29 21:31:39 +1100

    Added build order

--- CrackInterviews/C4/BuildOrder.cs
+++ CrackInterviews/C4/BuildOrder.cs
@@ -1,30 +1,121 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace C4
 {
-    internal class BuildOrder
+    public class BuildOrder
     {
-        internal static IList<string> Calculate(string[] project, IList<BuildDependency> dependencies)
+        public static IList<string> Calculate(string[] projects, IList<BuildDependency> dependencies)
         {
-            if (project == null) return new List<string>();
+            if (projects == null) return new List<string>();
 
-            var result = new List<string>();
-            return result;
+            var graph = new BuildGraph();
+
+            foreach (var p in projects)
+            {
+                graph.AddProject(p);                
+            }
+
+            foreach (var d in dependencies)
+            {
+                graph.AddEdge(d.ParentProjectName, d.DependentProjectName);
+            }
+
+            var result = new Stack<string>();
+
+            var hasCycle = false;
+            foreach (var pair in graph.ProjectMap)
+            {
+                if (pair.Value.Status != Status.Visited)
+                {
+                    hasCycle = hasCycle || BuildOrder.DoDFS(pair.Value, result);
+                    if (hasCycle) return new List<string>();
+                }
+            }
+
+            return result.ToList();
+        }
+
+        private static bool DoDFS(Project project, Stack<string> result)
+        {
+            // Circle detected
+            if (project.Status == Status.Visiting) return true;
+
+            // Project has been explored
+            if (project.Status == Status.Visited) return false;
+
+            // Start to process current project
+            project.Status = Status.Visiting;
+
+            var children = project.ChildrenProjectMap.Select(x => x.Value).ToList();
+            var hasCycle = false;
+            foreach (var p in children)
+            {
+                hasCycle = hasCycle || BuildOrder.DoDFS(p, result);
+                if (hasCycle) return true;
+            }
+
+            result.Push(project.ProjectName);
+            project.Status = Status.Visited;
+
+            return false;
+        }
+    }
+
+    public enum Status
+    {
+        New,
+
+        Visiting,
+
+        Visited
+    }
+
+    public class Project
+    {
+        public IDictionary<string, Project> ChildrenProjectMap { get; } = new Dictionary<string, Project>();
+
+        public string ProjectName { get; }
+
+        public Status Status { get; set; }
+
+        public Project(string projectName)
+        {
+            ProjectName = projectName;
+            Status = Status.New;
         }
     }
 
     public class BuildDependency
     {
-        public string DependentProject { get; }
+        public string ParentProjectName { get; }
 
-        public string Project { get; }
+        public string DependentProjectName { get; }
 
-        public BuildDependency(string dependentProject, string project)
+        public BuildDependency(string parentProjectName, string dependentProjectName)
         {
-            this.DependentProject = dependentProject;
-            this.Project = project;
+            this.ParentProjectName = parentProjectName;
+            this.DependentProjectName = dependentProjectName;
+        }
+    }
+
+    public class BuildGraph
+    {
+        public IDictionary<string, Project> ProjectMap { get; } = new Dictionary<string, Project>();
+
+        public void AddProject(string project)
+        {
+            var newProject = new Project(project);
+            this.ProjectMap.Add(newProject.ProjectName, newProject);
+        }
+
+        public void AddEdge(string parentProjectName, string dependentProjectName)
+        {
+            var parentProject = this.ProjectMap[parentProjectName];
+            var dependentProject = this.ProjectMap[dependentProjectName];
+            parentProject.ChildrenProjectMap.Add(dependentProject.ProjectName, dependentProject);
         }
     }
 
@@ -32,18 +123,68 @@ namespace C4
     public class BuildOrderTests
     {
         [TestCaseSource(nameof(GetTestData))]
-        public void Calculate_Test(string[] project, IList<BuildDependency> dependencies, IList<string> expectedResult)
+        public void Calculate_Test(string[] project, IList<BuildDependency> dependencies, bool hasCycle)
         {
             var result = BuildOrder.Calculate(project, dependencies);
-            for (int i = 0; i < result.Count; i++)
+            if (project == null || hasCycle)
             {
-                Assert.That(result[i], Is.EqualTo(expectedResult[i]));   
+                Assert.IsEmpty(result);
+                return;
+            }
+            
+            foreach (var d in dependencies)
+            {
+                var parentProjectIndex = -1;
+                var dependentProjectIndex = -1;
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (result[i] == d.ParentProjectName) parentProjectIndex = i;
+                    if (result[i] == d.DependentProjectName) dependentProjectIndex = i;
+                }
+                Assert.That(parentProjectIndex, Is.LessThan(dependentProjectIndex));
             }
         }
 
         private static IEnumerable<TestCaseData> GetTestData()
         {
-           yield return new TestCaseData(null, null, new List<string>());
+            yield return new TestCaseData(null, null, false);
+            
+            yield return new TestCaseData(
+                new [] {"1", "2", "3", "4"},
+                new List<BuildDependency>() { new BuildDependency("3", "4") },
+                false);
+            
+            yield return new TestCaseData(
+                new [] {"a", "b", "c", "d", "e", "f", "g", "h"},
+                new List<BuildDependency>()
+                {
+                    new BuildDependency("d", "g"),
+                    new BuildDependency("f", "c"),
+                    new BuildDependency("f", "b"),
+                    new BuildDependency("f", "g"),
+                    new BuildDependency("b", "h"),
+                    new BuildDependency("b", "e"),
+                    new BuildDependency("a", "e"),
+                    new BuildDependency("c", "a"),
+                },
+                false);
+            
+            yield return new TestCaseData(
+                new [] {"a", "b", "c", "d", "e", "f", "g", "h"},
+                new List<BuildDependency>()
+                {
+                    new BuildDependency("d", "g"),
+                    new BuildDependency("f", "c"),
+                    new BuildDependency("f", "b"),
+                    new BuildDependency("f", "g"),
+                    new BuildDependency("b", "h"),
+                    new BuildDependency("b", "e"),
+                    new BuildDependency("a", "e"),
+                    new BuildDependency("c", "a"),
+                    new BuildDependency("e", "d"),
+                    new BuildDependency("g", "f"),
+                },
+                true);
         }
     }
 }
