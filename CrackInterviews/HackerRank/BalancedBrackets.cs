7b090a96081b170c0b51300f89d82d51e18ec340
Haoming Yuan <haoming.yuan@cyara.com>
2020-12-22 23:19:54 +1100

    added balanced brackets

---
+++ CrackInterviews/HackerRank/BalancedBrackets.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace HackerRank
+{
+    public class BalancedBrackets
+    {
+        public static bool Calculate(string s)
+        {
+            if (s == null) return false;
+
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (IsLeftBracket(c)) stack.Push(c);
+                else
+                {
+                    if (stack.Count == 0) return false;
+                    var left = stack.Pop();
+                    if (!IsBracketMatching(left, c)) return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        private static bool IsLeftBracket(char c)
+        {
+            if (c == '{' || c == '[' || c == '(') return true;
+            return false;
+        }
+
+        private static bool IsBracketMatching(char left, char right)
+        {
+            if (left == '{' && right == '}') return true;
+            if (left == '[' && right == ']') return true;
+            if (left == '(' && right == ')') return true;
+            return false;
+        }
+    }
+
+    [TestFixture]
+    public class BalancedBracketsTests
+    {
+        [TestCase("{{}}{}", true)]
+        [TestCase("{{[[(())]]}}", true)]
+        [TestCase("{[(])}", false)]
+        [TestCase("{]{}", false)]
+        [TestCase("", true)]
+        [TestCase(null, false)]
+        public void CalculateTests(string s, bool expectedResult)
+        {
+            Assert.That(BalancedBrackets.Calculate(s), Is.EqualTo(expectedResult));
+        }
+    }
+}
