e0b3774ff066ead68b633a6c2f636a205499309b
Haoming Yuan <haominghyua29@gmail.com>
2023-03-18 16:26:12 +1100

    Add DP solution for PowerSet

--- CrackInterviews/C8/PowerSet.cs
+++ CrackInterviews/C8/PowerSet.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using NUnit.Framework;
 
@@ -6,23 +8,48 @@ namespace C8
 {
     public class PowerSet
     {
-        public static IList<HashSet<string>> Calculate(string[] set)
+        public static IList<IList<char>> CalculateWithDp(char[] set)
         {
-            if (set == null) return null;
+            Debug.Assert(set != null);
 
-            var result = new List<HashSet<string>>();
-            
-            if (set.Length == 0) return result;
-            var temp = new HashSet<string>();
-            
+            var results = new List<IList<char>>()
+            {
+                new List<char>()
+            };
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                var temp = results.ToList();
+                foreach (var t in temp)
+                {
+                    var newSet = t.ToList();
+                    newSet.Add(set[i]);
+                    results.Add(newSet);
+                }
+            }
+
+            return results;
+        }
+
+        public static IList<IList<char>> CalculateWithBackTrack(char[] set)
+        {
+            Debug.Assert(set != null);
+
+            var result = new List<IList<char>>();
+            // {
+            //     new List<char>()
+            // };
+
+            var temp = new List<char>();
+
             PowerSet.CalculateAux(result, temp, set, 0);
 
             return result;
         }
 
-        private static void CalculateAux(IList<HashSet<string>> result, HashSet<string> temp, string[] set, int index)
+        private static void CalculateAux(IList<IList<char>> result, IList<char> temp, char[] set, int index)
         {
-            result.Add(temp.Select(x => x).ToHashSet());
+            result.Add(temp.Select(x => x).ToList());
             for (int i = index; i < set.Length; i++)
             {
                 temp.Add(set[i]);
@@ -35,11 +62,24 @@ namespace C8
     [TestFixture]
     public class PowerSetTests
     {
-        [TestCaseSource(nameof(GetTestData))]
-        public void CalculateTest(string[] set, IList<HashSet<string>> expectedResult)
+        [TestCaseSource(nameof(GetDpTestData))]
+        public void CalculateWithDp_Test(char[] set, IList<IList<char>> expectedResult)
         {
-            var result = PowerSet.Calculate(set);
+            var result = PowerSet.CalculateWithDp(set);
 
+            AssertEqual(set, expectedResult, result);
+        }
+
+        [TestCaseSource(nameof(GetBackTrackTestData))]
+        public void CalculateWithBackTrack_Test(char[] set, IList<IList<char>> expectedResult)
+        {
+            var result = PowerSet.CalculateWithBackTrack(set);
+
+            AssertEqual(set, expectedResult, result);
+        }
+
+        private static void AssertEqual(char[] set, IList<IList<char>> expectedResult, IList<IList<char>> result)
+        {
             if (set == null)
             {
                 Assert.That(result, Is.EqualTo(expectedResult));
@@ -57,24 +97,63 @@ namespace C8
             }
         }
 
-        public static IEnumerable<TestCaseData> GetTestData()
+        public static IEnumerable<TestCaseData> GetBackTrackTestData()
         {
-            yield return new TestCaseData(null, null);
+            yield return new TestCaseData(Array.Empty<char>(), new List<IList<char>> {new List<char>()});
 
-            yield return new TestCaseData(new string[0], new List<HashSet<string>>());
-            
             yield return new TestCaseData(
-                new[] { "1", "2", "3" },
-                new List<HashSet<string>>
+                new[] {'1', '2', '3'},
+                new List<IList<char>>
                 {
-                    new HashSet<string> {},
-                    new HashSet<string> { "1" },
-                    new HashSet<string> { "1", "2" },
-                    new HashSet<string> { "1", "2", "3" },
-                    new HashSet<string> { "1", "3" },
-                    new HashSet<string> { "2" },
-                    new HashSet<string> { "2", "3" },
-                    new HashSet<string> { "3" }
+                    new List<char> { },
+                    new List<char> {'1'},
+                    new List<char> {'1', '2'},
+                    new List<char> {'1', '2', '3'},
+                    new List<char> {'1', '3'},
+                    new List<char> {'2'},
+                    new List<char> {'2', '3'},
+                    new List<char> {'3'}
+                });
+        }
+
+        public static IEnumerable<TestCaseData> GetDpTestData()
+        {
+            yield return new TestCaseData(Array.Empty<char>(), new List<IList<char>> {new List<char>()});
+
+            yield return new TestCaseData(
+                new[] {'1', '2', '3'},
+                new List<IList<char>>
+                {
+                    new List<char> { },
+                    new List<char> {'1'},
+                    new List<char> {'2'},
+                    new List<char> {'1', '2'},
+                    new List<char> {'3'},
+                    new List<char> {'1', '3'},
+                    new List<char> {'2', '3'},
+                    new List<char> {'1', '2', '3'}
+                });
+
+            yield return new TestCaseData(
+                new[] {'1', '2', '3', '4'},
+                new List<IList<char>>
+                {
+                    new List<char> { },
+                    new List<char> {'1'},
+                    new List<char> {'2'},
+                    new List<char> {'1', '2'},
+                    new List<char> {'3'},
+                    new List<char> {'1', '3'},
+                    new List<char> {'2', '3'},
+                    new List<char> {'1', '2', '3'},
+                    new List<char> {'4'},
+                    new List<char> {'1', '4'},
+                    new List<char> {'2', '4'},
+                    new List<char> {'1', '2', '4'},
+                    new List<char> {'3', '4'},
+                    new List<char> {'1', '3', '4'},
+                    new List<char> {'2', '3', '4'},
+                    new List<char> {'1', '2', '3', '4'}
                 });
         }
     }
