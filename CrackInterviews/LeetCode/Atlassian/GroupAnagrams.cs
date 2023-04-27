6a2984f269e56f4f4aff111eb1fa443b152187fd
Haoming Yuan <haominghyua29@gmail.com>
2023-04-27 20:43:09 +1000

    Create GroupAnagrams.cs

---
+++ CrackInterviews/LeetCode/Atlassian/GroupAnagrams.cs
@@ -0,0 +1,24 @@
+namespace LeetCode.Atlassian;
+
+public class GroupAnagrams
+{
+    public IList<IList<string>> RunGroupAnagrams(string[] strs)
+    {
+        var dic = new Dictionary<string, IList<string>>();
+
+        foreach (var s in strs)
+        {
+            var ss = string.Join("", s.OrderBy(c => c));
+            if (dic.ContainsKey(ss))
+            {
+                dic[ss].Add(s);
+            }
+            else
+            {
+                dic.Add(ss, new List<string>{ s });
+            }
+        }
+
+        return dic.Select(p => p.Value).OrderBy(v => v.Count).ToList();
+    }
+}
