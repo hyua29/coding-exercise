d2170d46a0c784a1b2c321f6ecd2d9a438b955fa
Haoming Yuan <haominghyua29@gmail.com>
2023-06-30 20:46:57 +0800

    Add MaximumAverageSubarrayI

---
+++ CrackInterviews/LeetCode/Leetcode75/MaximumAverageSubarrayI.cs
@@ -0,0 +1,19 @@
+namespace LeetCode.Leetcode75;
+
+public class MaximumAverageSubarrayI
+{
+    public double findMaxAverage(int[] nums, int k)
+    {
+        int max = 0;
+        for (int i = 0; i < k; i++)
+            max += nums[i];
+        int temp = max;
+        for (int i = k; i < nums.Length; i++)
+        {
+            temp = temp + nums[i] - nums[i - k];
+            max = Math.Max(temp, max);
+        }
+
+        return (max * 1.0) / k;
+    }
+}
