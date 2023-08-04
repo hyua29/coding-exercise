31d2858e315439b350fc0f4d9da12a4797c3ec49
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-04 22:19:34 +1000

    Create GuessNumberHigherOrLower.cs

---
+++ CrackInterviews/LeetCode/LeetCode75/GuessNumberHigherOrLower.cs
@@ -0,0 +1,39 @@
+﻿// /** 
+//  * Forward declaration of guess API.
+//  * @param  num   your guess
+//  * @return 	     -1 if num is higher than the picked number
+//  *			      1 if num is lower than the picked number
+//  *               otherwise return 0
+//  * int guess(int num);
+//  */
+//
+// public class Solution : GuessGame {
+//     public int GuessNumber(int n) {
+//
+//         var left = 1;
+//         var right = n;
+//
+//         var mid = left;
+//
+//         var result = 0;
+//         do
+//         {
+//             
+//             mid = left + (right - left) / 2;
+//            
+//             result = guess(mid);
+//
+//             if (result > 0)
+//             {
+//                 left = mid + 1;
+//             }
+//             else if (result < 0)
+//             {
+//                 right = mid - 1;
+//             }
+//         }   
+//         while (result != 0);
+//
+//         return mid;
+//     }
+// }
