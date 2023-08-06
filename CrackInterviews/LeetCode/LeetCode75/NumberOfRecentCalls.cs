ab2ed93042a1eed5bd047f023206e911f4c2ec8a
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-06 14:54:29 +1000

    RecentCounter

---
+++ CrackInterviews/LeetCode/LeetCode75/NumberOfRecentCalls.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class RecentCounter
+{
+    private readonly Queue<int> _queue;
+
+    public RecentCounter()
+    {
+        _queue = new Queue<int>();
+    }
+
+    public int Ping(int t)
+    {
+        _queue.Enqueue(t);
+
+        while (_queue.TryPeek(out var v) && t - v > 3000)
+        {
+            _queue.Dequeue();
+        }
+
+        return _queue.Count;
+    }
+}
