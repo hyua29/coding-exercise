07acbdde5dac199e0c62672859a2247d55142d9a
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-06 14:42:54 +1000

    CombinationSumIii

---
+++ CrackInterviews/LeetCode/LeetCode75/CombinationSumIii.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class CombinationSumIii
+{
+    public IList<IList<int>> CombinationSum3(int k, int n) {
+        var results = new List<IList<int>>();
+
+        Aux(k, n, 1, new List<int>(), results);
+
+        return results;
+    }
+
+    private void Aux(int k, int n, int position, IList<int> temp, IList<IList<int>> results)
+    {
+        if (temp.Sum() == n)
+        {
+            if (temp.Count == k)
+            {
+                results.Add(new List<int>(temp));
+            }
+
+            return;
+        }
+
+        for (int i=position; i<=9; i++)
+        {
+            if (temp.Sum() + i <= n && temp.Count <= k)
+            {
+                temp.Add(i);
+                Aux(k, n, i + 1, temp, results);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
