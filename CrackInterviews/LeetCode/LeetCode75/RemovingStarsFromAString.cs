a7dff6edc145c26ca3d4712aaf276ff5993d96be
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-06 14:43:00 +1000

    RemovingStarsFromAString

---
+++ CrackInterviews/LeetCode/LeetCode75/RemovingStarsFromAString.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class RemovingStarsFromAString
+{
+    public string RemoveStars(string s) {
+        var stack  = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (c != '*')
+            {
+                stack.Push(c);
+            } 
+            else
+            {
+                stack.Pop();
+            }
+        }
+
+        return string.Join(string.Empty, stack.Reverse());
+    }
+}
