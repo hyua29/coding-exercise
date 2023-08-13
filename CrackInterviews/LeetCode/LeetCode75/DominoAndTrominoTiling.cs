7427e53650943de266a05b825cae4f62083a7a0a
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-13 20:24:32 +1000

    Create DominoAndTrominoTiling.cs

---
+++ CrackInterviews/LeetCode/LeetCode75/DominoAndTrominoTiling.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.LeetCode75;
+
+public class DominoAndTrominoTiling
+{
+    public int NumTilings(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+
+        var buffer = new int[n + 1];
+        buffer[0] = 1;
+        buffer[1] = 1;
+        buffer[2] = 2;
+        buffer[3] = 5;
+        
+        for (int i = 4; i <= n; i++)
+        {
+            buffer[i] = (2*buffer[i-1] + buffer[i-3]) % 1000000007;
+        }
+
+        return buffer[n];
+    }
+}
