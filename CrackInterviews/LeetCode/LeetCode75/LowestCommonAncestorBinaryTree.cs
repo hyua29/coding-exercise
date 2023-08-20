78fda3ff99cf5458d1a669173eb137609041f3d0
Haoming Yuan <haoming.yuan@cyara.com>
2023-08-20 14:37:22 +1000

    LowestCommonAncestorBinaryTree

---
+++ CrackInterviews/LeetCode/LeetCode75/LowestCommonAncestorBinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace LeetCode.LeetCode75;
+
+/// <summary>
+/// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75
+/// </summary>
+public class LowestCommonAncestorBinaryTree
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        var node = Aux(root, p, q);
+
+        if (root == p)
+        {
+            return NodeFound(p, q) ? p : null;
+        }
+        else if (root == q)
+        {
+            return NodeFound(q, p) ? q : null;
+        }
+
+        return node;
+    }
+
+    private bool NodeFound(TreeNode current, TreeNode nodeToFind)
+    {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(current);
+
+        while (queue.TryDequeue(out var n))
+        {
+            if (n == nodeToFind)
+                return true;
+
+            if (n.left !=null)
+                queue.Enqueue(n.left);
+            
+            if (n.right !=null)
+                queue.Enqueue(n.right);
+        }
+
+        return false;
+    }
+
+    private TreeNode? Aux(TreeNode? root, TreeNode p, TreeNode q)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        if (root == p || root == q) return root;
+
+        var left = Aux(root.left, p, q);
+        var right = Aux(root.right, p, q);
+
+        if (left != null && right != null)
+        {
+            return root;
+        }
+
+        return left ?? right;
+    }
+}
--- CrackInterviews/LeetCode/LeetCode75/TreeNode.cs
+++ CrackInterviews/LeetCode/LeetCode75/TreeNode.cs
@@ -3,17 +3,11 @@ namespace LeetCode.LeetCode75;
 public class TreeNode
 {
     public int val;
-    public TreeNode left;
-    public TreeNode right;
+    public TreeNode? left;
+    public TreeNode? right;
 
-    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
     {
-        var valueKeeper = new Dictionary<int, int>();
-        foreach (var v in valueKeeper)
-        {
-            Console.WriteLine(v);
-        }
-
         this.val = val;
         this.left = left;
         this.right = right;
