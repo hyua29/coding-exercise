3aa6cadbb3c0c1f5ae5809388418fa7ac7a2b427
Haoming Yuan <haominghyua29@gmail.com>
2023-10-18 21:36:36 +1100

    SimplifyPathProblem

---
+++ CrackInterviews/LeetCode/Leetcode150/SimplifyPathProblem.cs
@@ -0,0 +1,60 @@
+namespace LeetCode.Leetcode150;
+
+public class SimplifyPathProblem
+{
+    public string SimplifyPath(string path)
+    {
+        var modifiedPath = path.Trim('/');
+
+        var buffer = new Stack<string>();
+        var entities = modifiedPath.Split('/', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var e in entities)
+        {
+            if (e.Equals("..", StringComparison.Ordinal))
+            {
+                buffer.TryPop(out _);
+                continue;
+            }
+
+            if (e != ".")
+            {
+                buffer.Push(e);
+            }
+        }
+
+        return $"/{string.Join('/', buffer.Reverse())}";
+    }
+}
+
+[TestFixture]
+public class SimplifyPathTests
+{
+    private SimplifyPathProblem _s = new SimplifyPathProblem();
+
+    [Test]
+    public void Test1()
+    {
+        string path = "/a/./b/../../c/";
+        string expected = "/c";
+        string simplifiedPath = _s.SimplifyPath(path);
+        Assert.That(simplifiedPath, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        string path = "/a/../../b/../c//.//";
+        string expected = "/c";
+        string simplifiedPath = _s.SimplifyPath(path);
+        Assert.That(simplifiedPath, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        string path = "/a//b////c/d//././/..";
+        string expected = "/a/b/c";
+        string simplifiedPath = _s.SimplifyPath(path);
+        Assert.That(simplifiedPath, Is.EqualTo(expected));
+    }
+}
